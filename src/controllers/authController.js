2ab5b26909e9ea3001e01b9308db848a70814929
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-05-12 21:54:49 +0200

    configuration files

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,33 @@
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
 const register = async (req, res)=>{
     const body = req.body;
+    const {name, email, password} = req.body;
+    
 
-    res.json(body)
-}
+    //check if user already exists
+   const userExists = await User.findOne({email})
+
+    if(userExists)
+        return res.status(400).json({message: "User already exists"})
+    
+
+    //hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //create user
+    const user = await User.create({ name, email, password: hashedPassword})
+    res.status(201).json({message: "Success",
+        data : {
+            user: {
+                id: user.id,
+                name: name,
+                email: email,
+            }
+        }
+        })
+    }
+
 
 export {register} 

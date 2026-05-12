b63a7245d32445f37b3837a71ba897542b5baeb5
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-05-12 08:41:57 +0200

    added models

--- src/server.js
+++ src/server.js
@@ -4,13 +4,19 @@ import {connectDB} from './config/db.js'
 
 //import routes
 import movieRoutes from './routes/movieRoutes.js'
+import authRoutes from './routes/authRoutes.js'
 config()
 connectDB()
 
 const app = express()
 
+//body parsing middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 //API Routes
 app.use("/movies", movieRoutes)
+app.use("/auth", authRoutes)
 
 const PORT = 5001;
 
